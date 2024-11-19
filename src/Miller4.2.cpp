e74bf8c37ebe27c8497cdc8a6c6655834a4c06f3
DavisMiller57 <155665409+DavisMiller57@users.noreply.github.com>
2024-11-19 08:34:03 -0600

    Added 4.2 assigment file

---
+++ src/Miller4.2.cpp
@@ -0,0 +1,29 @@
+#include "vex.h"
+#include <string>
+using std::string;
+using namespace vex;
+
+//Initializes motors, controller, and drivetrain
+motor armMotor = motor(PORT8, ratio18_1, false);
+motor clawMotor = motor(PORT3, ratio18_1, false);
+controller Controller = controller();
+motor left_motor = motor(PORT1, false);
+motor right_motor = motor(PORT10, true);
+drivetrain Drive = drivetrain(left_motor, right_motor, 259.34, 320, 40, mm, 1);
+
+//Simple print method to print a string to the brain screen
+void print(string text) {
+  Brain.Screen.print(text.c_str());
+}
+//Opens claw to a 160 degree angle
+void open() {
+  clawMotor.spinFor(reverse, 160, degrees);
+}
+//Closes claw from 160 degree angle
+void close() {
+  clawMotor.spinFor(160, degrees);
+}
+//Main program logic
+int main() {
+    
+}
