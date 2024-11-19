5a9ef067be70b66c9f5e20a5e613b5fed64ef8e5
DavisMiller57 <155665409+DavisMiller57@users.noreply.github.com>
2024-11-19 08:44:27 -0600

    Added new methods to 4.1

--- src/Miller4.1.cpp
+++ src/Miller4.1.cpp
@@ -7,15 +7,20 @@ motor armMotor = motor(PORT8, ratio18_1, false);
 motor clawMotor = motor(PORT3, ratio18_1, false);
 controller Controller = controller();
 motor left_motor = motor(PORT1, false);
-  motor right_motor = motor(PORT10, true);
-  
-
-  // Creates drivetrain
+motor right_motor = motor(PORT10, true);
 drivetrain Drive = drivetrain(left_motor, right_motor, 259.34, 320, 40, mm, 1);
 
 //Simple print method to print a string to the brain screen
 void print(string text) {
   Brain.Screen.print(text.c_str());
+}
+//Raises arm to a 160 degree angle
+void raise() {
+  armMotor.spinFor(160, degrees);
+}
+//Lowers arm from a 160 degree angle
+void lower() {
+  armMotor.spinFor(reverse, 160, degrees);
 }
 //Opens claw
 void open() {
