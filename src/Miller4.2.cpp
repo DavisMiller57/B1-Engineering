993d89d5c2be4bef8e983196c876a8197bede1be
DavisMiller57 <155665409+DavisMiller57@users.noreply.github.com>
2024-11-19 08:43:29 -0600

    Created inital attempt

--- src/Miller4.2.cpp
+++ src/Miller4.2.cpp
@@ -15,6 +15,14 @@ drivetrain Drive = drivetrain(left_motor, right_motor, 259.34, 320, 40, mm, 1);
 void print(string text) {
   Brain.Screen.print(text.c_str());
 }
+//Raises arm to a 160 degree angle
+void raise() {
+  armMotor.spinFor(160, degrees);
+}
+//Lowers arm from a 160 degree angle
+void lower() {
+  armMotor.spinFor(reverse, 160, degrees);
+}
 //Opens claw to a 160 degree angle
 void open() {
   clawMotor.spinFor(reverse, 160, degrees);
@@ -23,7 +31,22 @@ void open() {
 void close() {
   clawMotor.spinFor(160, degrees);
 }
-//Main program logic
+//Main program logic - Autonomous Retrial
 int main() {
-    
+    //Sets claw open prior to leaving starting position
+    open();
+    //Drives foward just over 3 feet to account for any error
+    Drive.driveFor(40, inches);
+    //redundant stop of robot motion 
+    Drive.stop();
+    //Closes claw to grab object
+    close();
+    //Raises claw
+    raise();
+    //Full spin to turn around, using 135º from 4.1 due to robot processing errors
+    Drive.turnFor(right, 135, degrees);
+    //Drives forward to return to starting position, adding on 4 inches to account for error
+    Drive.driveFor(44, inches);
+    //Opens claw to drop ball
+    open();
 }
