776036da78bebb30b46e4490e76c5646edb1416d
DavisMiller57 <155665409+DavisMiller57@users.noreply.github.com>
2024-11-19 08:58:40 -0600

    Added libary file and removed methods/inits from program files

--- src/Miller4.1.cpp
+++ src/Miller4.1.cpp
@@ -1,35 +1,9 @@
 #include "vex.h"
 #include <string>
+#include "lib.cpp"
 using std::string;
 using namespace vex;
 
-motor armMotor = motor(PORT8, ratio18_1, false);
-motor clawMotor = motor(PORT3, ratio18_1, false);
-controller Controller = controller();
-motor left_motor = motor(PORT1, false);
-motor right_motor = motor(PORT10, true);
-drivetrain Drive = drivetrain(left_motor, right_motor, 259.34, 320, 40, mm, 1);
-
-//Simple print method to print a string to the brain screen
-void print(string text) {
-  Brain.Screen.print(text.c_str());
-}
-//Raises arm to a 160 degree angle
-void raise() {
-  armMotor.spinFor(160, degrees);
-}
-//Lowers arm from a 160 degree angle
-void lower() {
-  armMotor.spinFor(reverse, 160, degrees);
-}
-//Opens claw
-void open() {
-  clawMotor.spinFor(reverse, 160, degrees);
-}
-//Closes claw
-void close() {
-  clawMotor.spinFor(160, degrees);
-}
 int main() {
   for(int i = 0; i <= 4; i++) {
     Drive.driveFor(12, inches);
